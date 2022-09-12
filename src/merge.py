ad45fadef396730bf0a3ca5c591fabe108530c9a
AneNaur <202008164@post.au.dk>
2022-09-12 12:06:55 +0200

    merge

--- src/merge.py
+++ src/merge.py
@@ -1,7 +1,7 @@
 """Code for merging two sorted lists."""
 
 
-def merge(x: list[int], y: list[int]) -> list[int]:
+def merge(x,y): #x: list[int], y: list[int]) -> list[int]:
     """
     Merge two sorted lists.
 
@@ -16,7 +16,26 @@ def merge(x: list[int], y: list[int]) -> list[int]:
     # FIXME: fill out the loop so you merge the lists
     # until one of them is empty
     while i < len(x) and j < len(y):
-        break  # FIXME: you shouldn't just break here
+        if x[i] != y[j]:
+            if x[i] < y[j]:
+                z.append(x[i])
+                i+=1
+            else:
+                z.append(y[j])
+                j+=1
+        else:
+            z.append(x[i])
+            i+=1
+            j+=1
+    if i == len(x):
+        z=z+y[j:]
+    elif j == len(y):
+        z=z+x[i:]
+        #break  # FIXME: you shouldn't just break here
     # At least one of the lists is empty now. Copy the
     # remainder of the other into z.
     return z
+
+#a=[0,2,3,4,6,8,10,11]
+#b=[1,5,6,7,9,10,11,12,13]
+#print(merge(a,b))
